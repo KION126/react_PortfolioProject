8ee0cc381c53c6035946f6bd017c95013b923694
WangGeon <73977410+KION126@users.noreply.github.com>
2024-10-30 23:04:38 +0900

    feature: Welcom컴포넌트 구현

--- src/component/contents/Welcome.jsx
+++ src/component/contents/Welcome.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import IMG_LOGO_SHADOW from '../../image/logo_shadow.svg';
 
 const Welcome = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p>This is the Welcome component.</p>
+        <div className='flex flex-col h-full items-center pt-[150px] font-en'>
+            <img src={IMG_LOGO_SHADOW} alt='logo'
+                className='size-[200px]'
+            />
+            <p className='text-2xl font-semibold mt-6'>Welcome!</p>
+            <p className='flex text-4xl font-bold mt-2'>
+                <span className='text-[#A48ACF]'>WangGun's</span>&nbsp;Studio
+            </p>
         </div>
     );
 };
