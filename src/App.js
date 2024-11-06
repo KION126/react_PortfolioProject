b4f7a826556fe6e66a0312b1bc54085b4f079006
WangGeon <73977410+KION126@users.noreply.github.com>
2024-11-06 14:54:39 +0900

    Chore: BrowserRouter 수정

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
         <Routes>
           <Route 
             path="/" 
-            element={<Main />} />
+            element={<Main />}
+            basename={process.env.PUBLIC_URL} />
         </Routes>
       </Router>
     </RecoilRoot>
